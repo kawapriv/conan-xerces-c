957b02d6fa796d1ef8c1e63677564a74a6e821d6
Karl Wallner <kwallner@mail.de>
2018-07-07 17:27:13 +0200

    Another/better solution to find ICU (installed by anaconda python)

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,8 @@
 import os
+from shutil import copyfile
+
 from conans import ConanFile, tools, CMake
+from conans.util import files
 
 class XercesCConan(ConanFile):
     name = "xerces-c"
@@ -18,15 +21,19 @@ class XercesCConan(ConanFile):
         tools.get("http://ftp-stud.hs-esslingen.de/pub/Mirrors/ftp.apache.org/dist//xerces/c/3/sources/%s-%s.tar.gz"  % (self.name, self.version))
         os.rename("%s-%s" % (self.name, self.version), self.source_subfolder)
         
-        # No ICU support
-        tools.replace_in_file("sources/CMakeLists.txt",
-            'include(XercesICU)', 'set(ICU_FOUND False)')
-
+        # Avoid finding ICU from Anaconda python which can not be used
+        tools.replace_in_file("%s/%s/cmake/XercesTranscoderSelection.cmake" % (self.source_folder, self.source_subfolder),
+            'if(ICU_FOUND)',
+            'if(ICU_FOUND AND NOT WIN32)')
+        
     def build(self):
         cmake = CMake(self)
         cmake.configure(source_dir="%s/%s" % (self.source_folder, self.source_subfolder))
         cmake.build()
         cmake.install()
                
+    def package(self):
+        pass
+
     def package_info(self):
         self.env_info.XercesC_DIR = self.package_folder
